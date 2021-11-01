c9e29155ca44071d03d7d45ef578337e99620316
UsernamesAreNotMyThing <aaklien16@gmail.com>
2021-11-01 15:28:36 -0400

    Finished
    It should be essentially finished at this point

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Eyeballs 👁
 The black portion will follow the mouse cursor
 
-**How to use**
+> This page is primarily intended to be evaluated for an online course I'm currently taking, however you can still play around with it if you want
+
+## How to use
 + Play around with moving the cursor around the page and watch the eyes follow it
 + *Right-click* to add an additional eyeball where the cursor is
-+ *Press __R__* to roll the eyes
+
+### Additional actions:
+Key			|	Action
+---			|	---
+Left-Click	|	Add an eyeball
+**R**		|	Roll eyes
+**S**		|	Spin horizontally
+**B**		|	Blink eyes
--- eyes.js
+++ eyes.js
@@ -1,9 +1,8 @@
-
 /**
- * 
- * @param {number | string} value 
- * @param {number | string} min Minimum value
- * @param {number | string} max Maximum value
+ * @param {number | string}	value	Clamp this between  `min` and `max`
+ * @param {number | string}	min		Minimum value
+ * @param {number | string}	max		Maximum value
+ * @returns {number | string}	Clamped value between `min` and `max`
  */
 function clamp(value, min, max) {
 	if (value < min) return min;
@@ -14,22 +13,30 @@ function clamp(value, min, max) {
 class Eyeball {
 	/**
 	 * 
-	 * @param {number} positionX 
-	 * @param {number} positionY 
+	 * @param {number | {x: number; y: number}} positionX + For `Number`: Starting X position (1.0 = 100% of `window.innerWidth`)
+	 * @param {number} positionY Starting Y position (1.0 = 100% of `window.innerHeight`)
 	 */
 	constructor(positionX, positionY) {
-		this.x = positionX;
-		this.y = positionY;
+		/** @type Number*/
+		this.x = typeof positionX == 'number' ? positionX : positionX.x;
+		/** @type Number*/
+		this.y = typeof positionX == 'number' ? positionY : positionX.y;
 		var element = document.createElement('div');
 		element.className = 'eyes';
 		element.innerHTML = "<div class = 'eye'><div class = 'ball'/></div>";
 		element.style.left = `${(positionX - 120 / innerWidth) * 100}%`;
 		element.style.top = `${positionY * 100}%`;
 		document.body.appendChild(element);
+		/** The pupil
+		 * @type HTMLElement
+		 */
 		this.element = element.children[0].children[0];
+		/** @type boolean */
 		this.__busy = false;
 	}
-	/** @param {MouseEvent} event */
+	/** Adjusts the pupil's position to "look" at the cursor
+	 * @param {MouseEvent} event
+	 */
 	update(event) {
 		if (this.__busy) return;
 		const angle = Math.atan2(event.clientY - innerHeight * this.y, event.clientX - innerWidth * this.x);
@@ -37,6 +44,7 @@ class Eyeball {
 		this.element.style.left = `${50 + clamp(dist, 0, 25) * Math.cos(angle)}%`;
 		this.element.style.top = `${50 + clamp(dist, 0, 10) * Math.sin(angle)}%`;
 	}
+	/** initiates the "eye-roll" animation */
 	roll() {
 		if (this.__busy) return;
 		this.__busy = true;
@@ -50,18 +58,91 @@ class Eyeball {
 		};
 		setTimeout(f, 50);
 	}
+	rollHorizontal() {
+		if (this.__busy) return;
+		this.__busy = true;
+		let offset = 0;
+		this.element.style.top = '50%';
+		const f = () => {
+			this.element.style.left = `${50 + 25 * Math.cos(offset / 57.296)}%`;
+			if ((offset += 3) < 360) setTimeout(f, 50);
+			else this.__busy = false;
+		}; setTimeout(f, 50);
+	}
+	/** Initiates the blinking animation */
+	blink() {
+		if (this.__busy) return;
+		this.__busy = true;
+		let step = 0;
+		const f = () => {
+			step++;
+			const a = step <= 25 ? step * 2 : 100 - step * 2;
+			const b = step <= 25 ? 100 - 2 * step : step * 2;
+			this.element.parentElement.style['-webkit-mask-image'] = `linear-gradient(#00000000 ${a}%, white ${a}%, white ${b}%, #00000000 ${b}%)`;
+			if (step < 50) setTimeout(f, 50);
+			else this.__busy = false;
+		};
+		setTimeout(f, 50);
+	}
+	get busy() {
+		return this.__busy;
+	}
+	get position() {
+		return {x: this.x, y: this.y};
+	}
+	/** @param value Should have `x` and `y` properties */
+	set position(value) {
+		if (value == null || value.x == undefined || value.y == undefined) return;
+		this.x = value.x;
+		this.y = value.y;
+		this.element.parentElement.style.left = this.x;
+	}
+	get style() {
+		return this.element.style;
+	}
 }
-
-const balls = [new Eyeball(.5, .5)];
+/** @type Eyeball[] */
+const balls = [];
+balls.roll = function() {
+	this.forEach(i => i.roll());
+}
+/**
+ * @param {MouseEvent} event
+ * @type {(MouseEvent) => void}
+*/
+balls.update = function(event) {
+	this.forEach(i => i.update(event));
+}
+/**
+ * @type {() => void}
+ * @param {KeyboardEvent} event 
+ */
+balls.rollHorizontal = function(event) {
+	this.forEach(i => i.rollHorizontal(event));
+}
+balls.blink = function() {
+	this.forEach(i => i.blink());
+}
 
 document.onmousemove = function (event) {
-	balls.forEach(b => b.update(event));
+	balls.update(event);
 };
-document.oncontextmenu = function (event) {
+document.onclick = function (event) {
 	balls.push(new Eyeball(event.clientX / innerWidth, event.clientY / innerHeight));
+	document.getElementById('empty-message').style.display = 'none';
 }
 document.onkeydown = function(event) {
-	if (event.key == 'r' || event.key == 'R') {
-		balls.forEach(b => b.roll());
+	switch (event.key) {
+		case 'r':
+		case 'R':
+			balls.roll();
+			break;
+		case 's':
+		case 'S':
+			balls.rollHorizontal();
+			break;
+		case 'b':
+		case 'B':
+			balls.blink();
 	}
 }
